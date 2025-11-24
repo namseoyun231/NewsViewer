fa153885a469ebf1a15781789516749feb266eed
남서윤 <namseoyun231@gmail.com>
2025-11-24 16:48:09 +0900

    6개 파일 추가

---
+++ src/components/Categories.tsx
@@ -0,0 +1,45 @@
+import styles from "./Categories.module.css";
+import { NavLink } from "react-router";
+
+const categories = [
+  {
+    name: "all",
+    text: "전체보기",
+  },
+  {
+    name: "business",
+    text: "비즈니스",
+  },
+  {
+    name: "entertainment",
+    text: "엔터테인먼트",
+  },
+  {
+    name: "health",
+    text: "건강",
+  },
+  {
+    name: "science",
+    text: "과학",
+  },
+  {
+    name: "sports",
+    text: "스포츠",
+  },
+  {
+    name: "technology",
+    text: "기술",
+  },
+];
+
+export default function Categories() {
+  return (
+    <div className={styles.categoryBlock}>
+      {categories.map((c) => (
+        <NavLink key={c.name} className={styles.category} to={}>
+          {c.text}
+        </NavLink>
+      ))}
+    </div>
+  );
+}
---
+++ src/components/NewsItem.tsx
@@ -0,0 +1,34 @@
+import styles from "./NewsItem.module.css";
+
+export default function NewsItem({
+  article,
+}: {
+  article: {
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+  };
+}) {
+  const { title, description, url, urlToImage } = article;
+  return (
+    <div className={styles.newsItem}>
+      {urlToImage && (
+        <div className={styles.thumbnail}>
+          <a href={url} target="_blank" rel="noopener noreferrrer">
+            <img src={urlToImage} alt="thumbnail" />
+          </a>
+        </div>
+      )}
+
+      <div className={styles.contents}>
+        <h2>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </h2>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
